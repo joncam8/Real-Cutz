77edadb2c486dd390ad77aa3c3bb469cbe19f8db
Jonathan Cameron Jr <joncam7237@att.net>
2020-01-08 21:26:36 -0600

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+	$("#nav_menu").click(function(){
+		$("#navlist").slideToggle(1000);
+	});						   
+});
+
+<!--
+var sprycheckbox1 = new Spry.Widget.ValidationCheckbox("sprycheckbox1");
+//-->
